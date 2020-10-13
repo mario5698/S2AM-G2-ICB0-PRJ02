cc01ca83830ccdac4aa9fdc248ba97ac2d867dcf
g2nik <nikita.chechel@sarria.salesians.cat>
2020-10-13 15:57:16 +0200

    XD 5

--- SecureCore/Mantenimiento.Designer.cs
+++ SecureCore/Mantenimiento.Designer.cs
@@ -54,9 +54,9 @@
             this.lblText.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblText.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblText.ForeColor = System.Drawing.Color.White;
-            this.lblText.Location = new System.Drawing.Point(0, 181);
+            this.lblText.Location = new System.Drawing.Point(0, 138);
             this.lblText.Name = "lblText";
-            this.lblText.Size = new System.Drawing.Size(350, 269);
+            this.lblText.Size = new System.Drawing.Size(350, 312);
             this.lblText.TabIndex = 1;
             this.lblText.Text = "Este rincón del programa no se encuentra disponible.\r\nPor ahora...";
             this.lblText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -68,7 +68,7 @@
             this.lblTitle.ForeColor = System.Drawing.Color.White;
             this.lblTitle.Location = new System.Drawing.Point(0, 0);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(350, 181);
+            this.lblTitle.Size = new System.Drawing.Size(350, 138);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Mantenimiento";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
